5ba9e2c10b6f2ea11b5f5bb7c57e8fc73150c469
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2023-02-08 16:08:21 +1100

    simplify path for both absolute and relative path

--- simplifyPath.cpp
+++ simplifyPath.cpp
@@ -7,33 +7,53 @@ using namespace std;
 string simplify(string path) {
     istringstream iss(path);
 
+    // filtering
+    vector<string> tokens;
     string token;
-    vector<string> stack;
     while (getline(iss, token, '/')) {
-        if ( (token == ".") || (token == "") ) {
+        if (token == "." || token == "") {
             continue;
         }
-
-        if (token == "..") {
-            if (stack.size() != 0) {
-                stack.pop_back();
-            }
-            continue;
-        }
-        stack.push_back(token);
+        tokens.push_back(token);
     }
 
-    for (string s: stack) cout << "/" << s;
-    cout << endl;
+    vector<string> stack;
+    if (path[0] == '/') {
+        stack.push_back("");
+    }
 
-    return " ";
+    for (auto token: tokens) {
+        if (token == "..") {
+            if ( (stack.size() == 0) || (stack[stack.size()-1] == "..") ) {
+                stack.push_back("..");
+            }
+            else if (stack[stack.size()-1] != "") {
+                stack.pop_back();
+            }
+        }
+        else stack.push_back(token);
+    }
+
+    if (stack.size() == 1 || stack.size() == 0) return "/";
+
+    int i = 0;
+    ostringstream oss;
+    for (auto token: stack) {
+        if (i != 0) {
+            oss << "/";
+        }
+        i++;
+        oss << token;
+    }
+
+    return oss.str();
 }
 
 int main(void) {
     string path = "/../x/y/../z/././w/a///../c/./";
     //   x/z/w/c
 
-    simplify(path);
+    cout << simplify(path) << endl;
 
     return 0;
 }
